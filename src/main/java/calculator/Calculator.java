1222a48cc8a2c232644814a7dc686c08feb6705c
Ashk Khademian <ashkan.khd.q@gmail.com>
2023-08-04 21:17:23 +0330

    fix: provide CalculatorInterface in the Calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,23 @@
 package calculator;
 
-public class Calculator {
+public class Calculator implements CalculatorInterface{
+    @Override
     public int add(int a, int b) {
         return a + b;
+    }
+
+    @Override
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    @Override
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    @Override
+    public int pow(int a, int b) {
+        return (int) Math.pow(a, b);
     }
 }
