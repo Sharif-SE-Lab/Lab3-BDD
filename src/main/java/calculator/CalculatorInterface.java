fda3202d116e994e7549e1dbb4a50ae20b146430
Ashk Khademian <ashkan.khd.q@gmail.com>
2023-08-04 21:16:51 +0330

    feat: add tests corresponding to the scenarios and a new interface for the calculator

---
+++ src/main/java/calculator/CalculatorInterface.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public interface CalculatorInterface {
+    int add(int a, int b);
+
+    int mul(int a, int b);
+
+    int div(int a, int b);
+
+    int pow(int a, int b);
+}
